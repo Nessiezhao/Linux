ad8ed82b1a7496da3083ec3e42acba71bd750c6e
Nessiezhao <Nessie_zhao@163.com>
2018-06-14 20:36:19 +0800

    add HTTP

--- HTTP/httpd.c
+++ HTTP/httpd.c
@@ -5,12 +5,13 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<pthread.h>
 
 static void usage(const char* proc)
 {
     printf("Usage:%s port\n",proc);//告诉客户端应该如何使用
 }
-int startup(int port)
+static int startup(int port)
 {
     int sock = socket(AF_INET,SOCK_STREAM,0);
     if(sock < 0)//创建失败
@@ -18,6 +19,11 @@ int startup(int port)
         perror("socket");
         exit(2);
     }
+    int opt = 1;
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+    //第一个参数要设置的套接字
+    //第二个参数要设置的层数
+    //第三个是要干什么，现在的情况是需要端口可以复用
     struct sockaddr_in local;//填充本地消息
     local.sin_family = AF_INET;
     local.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -34,6 +40,10 @@ int startup(int port)
     }
     return sock;
 }
+static void* handler_request(void* arg)
+{
+    int sock = (int)arg;
+}
 int main(int argc,char* argv[])
 {
     if(argc != 2)
@@ -43,4 +53,19 @@ int main(int argc,char* argv[])
     }
     //现在的Http服务器底层是基于Tcp的，所以第一件事一定要有一个listen_sock
     int listen_sock = startup(atoi(argv[1]));//调用start来获得监听套接字
+    //监听套接字有了就进行事件处理
+    for(;;)
+    {
+        struct sockaddr_in client;
+        socklen_t len = sizeof(client);
+        int new_sock = accept(listen_sock,(struct sockaddr*)&client,&len);//获取连接
+        if(new_sock < 0)
+        {
+            perror("accept");
+            continue;
+        }
+        pthread_t id;
+        pthread_create(&id,NULL,handler_request,(void *)new_sock);
+        pthread_detach(id);
+    }
 }
